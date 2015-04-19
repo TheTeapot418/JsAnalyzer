8121829e113ad36567119939bca3bd150d9e0828
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2015-04-19 12:57:29 +0200

    Working analyzer

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var scene, ctx, audioCtx, analyzer, source;
+var data = new Uint8Array(2048);
+var gain = 1;
 scene = document.getElementById("scene");
 ctx = scene.getContext('2d');
 
@@ -8,16 +10,27 @@ analyzer.fftSize = 2048;
 
 
 function mainLoop() {
-	console.log("frame");
-	var distance = 1;
-	var data = new Uint8Array(2048);
+	var sum;
+	var average;
 	analyzer.getByteFrequencyData(data);
 	ctx.strokeStyle = "#DDD";
+	ctx.lineWidth = 1;
 	
 	window.requestAnimationFrame(mainLoop);
 	
+	ctx.beginPath();
 	ctx.clearRect(0, 0, scene.width, scene.height);
+	ctx.moveTo(0, scene.height - data[0]*gain);
 	
+	for (var i = 2; i < scene.width; i+= (data.length / scene.width)) {
+		ctx.lineTo(Math.floor(i), Math.floor(scene.height - data[Math.floor(i)]*gain));
+	}
+	
+//	for (var i = 1; i < data.length; i++) {
+//		ctx.lineTo(i, scene.height - data[i]);
+//	}
+	ctx.stroke();
+	ctx.closePath();
 }
 
 
@@ -29,3 +42,7 @@ navigator.webkitGetUserMedia({audio: true}, function(mediaStream) {
 }, function(err) {
 	console.log(err);
 });
+
+function changeGain(val) {
+	gain = val;
+}
