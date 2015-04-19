4713d1baeef5b4c34a9429b80f36ca6e0a5560d8
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2015-04-19 14:26:49 +0200

    Testing some things ...

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 var scene, ctx, audioCtx, analyzer, source;
 var data = new Uint8Array(2048);
 var gain = 1;
+var analyzerStart = 0;
+var analyzerEnd = 4096*2;
 scene = document.getElementById("scene");
 ctx = scene.getContext('2d');
 
 //Creating audio context
 audioCtx = new AudioContext();
 analyzer = audioCtx.createAnalyser();
-analyzer.fftSize = 2048;
+analyzer.fftSize = 4096*2;
 
 //This function will be executed 60 times per second
 function mainLoop() {
@@ -25,9 +27,9 @@ function mainLoop() {
 	//Drawing the spectrum for the current frame
 	ctx.beginPath();
 	ctx.clearRect(0, 0, scene.width, scene.height);
-	ctx.moveTo(0, scene.height - data[0]*gain);
+	ctx.moveTo(0, scene.height - data[analyzerStart]*gain);
 	
-	for (var i = 2; i < scene.width; i+= (data.length / scene.width)) {
+	for (var i = (analyzerStart + 1); i < scene.width; i+= (scene.width / analyzerEnd)) {
 		ctx.lineTo(Math.floor(i), Math.floor(scene.height - data[Math.floor(i)]*gain));
 	}
 	
