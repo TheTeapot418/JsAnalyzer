2b55df0c2611e94dcb5636f5ce1be41889674ad0
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2015-04-19 13:08:02 +0200

    Added som doc and comments

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
+//Setting up variables
 var scene, ctx, audioCtx, analyzer, source;
 var data = new Uint8Array(2048);
 var gain = 1;
 scene = document.getElementById("scene");
 ctx = scene.getContext('2d');
 
+//Creating audio context
 audioCtx = new AudioContext();
 analyzer = audioCtx.createAnalyser();
 analyzer.fftSize = 2048;
 
-
+//This function will be executed 60 times per second
 function mainLoop() {
 	var sum;
 	var average;
+	//Fetching data from the analyzer
 	analyzer.getByteFrequencyData(data);
 	ctx.strokeStyle = "#DDD";
 	ctx.lineWidth = 1;
 	
+	//Requesting next frame to the navigator (enables GPU accelerated graphics)
 	window.requestAnimationFrame(mainLoop);
 	
+	//Drawing the spectrum for the current frame
 	ctx.beginPath();
 	ctx.clearRect(0, 0, scene.width, scene.height);
 	ctx.moveTo(0, scene.height - data[0]*gain);
@@ -26,23 +31,23 @@ function mainLoop() {
 		ctx.lineTo(Math.floor(i), Math.floor(scene.height - data[Math.floor(i)]*gain));
 	}
 	
-//	for (var i = 1; i < data.length; i++) {
-//		ctx.lineTo(i, scene.height - data[i]);
-//	}
 	ctx.stroke();
 	ctx.closePath();
 }
 
-
+//Requesting access to the user's microphone
 navigator.webkitGetUserMedia({audio: true}, function(mediaStream) {
+	
+	//Connecting the audio stream to the analizer
 	source = audioCtx.createMediaStreamSource(mediaStream);
 	source.connect(analyzer);
-	
 	mainLoop();
 }, function(err) {
 	console.log(err);
 });
 
+
+//Callback function for the range input
 function changeGain(val) {
 	gain = val;
 }
